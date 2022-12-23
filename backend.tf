0f34ed242432698abe4b9b24349b8733fba17d0b
oluseyi88 <oluseyisunnyokundia@gmail.com>
2022-12-22 23:02:42 -0500

    removed backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform-rg"
-    storage_account_name = "terraformstoreacck"
-    container_name       = "terraform"
-    key                  = "oluseyi.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "terraform-rg"
+#     storage_account_name = "terraformstoreacck"
+#     container_name       = "terraform"
+#     key                  = "oluseyi.tfstate"
+#   }
+# }
